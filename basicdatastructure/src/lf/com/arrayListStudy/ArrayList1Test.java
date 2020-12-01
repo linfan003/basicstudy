2f0f12f6c8a08610f5b7761f32ccc168c7f1799c
linfan003 <linfan0022@gmail.com>
2020-12-01 15:49:06 +0800

    list的元素删除

--- basicdatastructure/src/lf/com/arrayListStudy/ArrayList1Test.java
+++ basicdatastructure/src/lf/com/arrayListStudy/ArrayList1Test.java
@@ -3,6 +3,7 @@ package lf.com.arrayListStudy;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -25,16 +26,17 @@ public class ArrayList1Test {
     }
     private static  String target = "hangzhou";
 
+
     private static void print(List<String> list){
         for (String item : list) {
             System.out.println("元素值：" + item);
         }
     }
 
-    @Test
     /**
-     *
+     * 无法重复删除
      */
+    @Test
     public  void remove1() {
         for (String str : list){
             if (target.equals(str)){
@@ -44,10 +46,67 @@ public class ArrayList1Test {
         }
         print(list);
     }
+    /**
+     * 错误
+     */
     @Test
     public  void remove2() {
-        int size = list.size();
-        for(int i = 0; i < size; i++){
+        for (String str : list){
+            if (target.equals(str)){
+                list.remove(str);
+            }
+        }
+        print(list);
+    }
+    /**
+     * 错误，在删除时会删除指定下标的元素，但是整个列表会前移，造成元素遗漏
+     */
+    @Test
+    public  void remove3() {
+        for(int i = 0; i < list.size(); i++){
+            String item = list.get(i);
+            if("shanghai".equals(item)){
+                list.remove(item);
+            }
+        }
+        print(list);
+    }
+    /**
+     * 会造成异常
+     *  删除 末尾 的元素，会造成 modCount 和 expectedModCount 的不一致导致异常抛出。
+     */
+    @Test
+    public  void remove4() {
+        for(int i = 0; i < list.size(); i++){
+            String item = list.get(i);
+            if(target.equals(item)){
+                list.remove(item);
+            }
+        }
+        print(list);
+    }
+
+    /**
+     * 迭代器删除
+     */
+    @Test
+    public  void remove5() {
+        Iterator<String> iter = list.iterator();
+        while (iter.hasNext()) {
+            String item = iter.next();
+            if (item.equals(target)) {
+                iter.remove();
+            }
+        }
+        print(list);
+    }
+
+    /**
+     * 倒序删除，不会造成元素的遗漏
+     */
+    @Test
+    public  void remove6() {
+        for(int i = list.size() - 1; i >= 0; i--){
             String item = list.get(i);
             if(target.equals(item)){
                 list.remove(item);
